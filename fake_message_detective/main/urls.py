700c3092340df0ddcb581944d1579994b4e5e7f4
yohan11 <yipco77@naver.com>
2023-05-15 02:28:31 +0900

    feat(#5): 피싱 의심 메시지 클릭시 해당 경고가 유효한지 확인하는 모달창 노출 및 각 버튼 클릭 시 결과 기록 구현

--- fake_message_detective/main/urls.py
+++ fake_message_detective/main/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('messagedatas/', views.getMessageDatas, name="messagedatas"),
-    path('inputform/', views.inputform, name='inputform') 
+    path('inputform/', views.inputform, name='inputform'),
+    path('inputInvalid/', views.inputInvalid, name='inputInvalid') ,
+    path('inputValid/', views.inputValid, name='inputValid') 
 ]
--- fake_message_detective/main/views.py
+++ fake_message_detective/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Message
+from .models import Message, Warning
 # Create your views here.
 def index(request):
     messageList = Message.objects.all()
@@ -8,7 +8,6 @@ def index(request):
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Message
 from .serializers import MessageSerializer
 
 @api_view(['GET'])
@@ -17,15 +16,30 @@ def getMessageDatas(request):
     serializer = MessageSerializer(datas, many=True)
     return Response(serializer.data)
 
-
-from .models import Message
 from datetime import datetime
 
 def inputform(request):
     if request.method == 'POST':
         std = Message()
+        std.user='주녕'
         std.message_content = request.POST['message_content']
         std.message_sent_time = datetime.now()
+        std.save()
+
+    return redirect('/')
+
+def inputInvalid(request):
+    if request.method == 'POST':
+        std =Warning()
+        std.warning_valid=0
+        std.save()
+
+    return redirect('/')
+
+def inputValid(request):
+    if request.method == 'POST':
+        std =Warning()
+        std.warning_valid=1
         std.save()
 
     return redirect('/')
